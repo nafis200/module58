b33ce451c3d6535b13fa4dcf97fdc66ca6db43e8
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-06 20:36:50 +0600

    project final

--- src/components/layout/pages/useServices.jsx
+++ src/components/layout/pages/useServices.jsx
@@ -16,3 +16,22 @@ const useServices = () => {
 };
 
 export default useServices;
+
+
+
+// import { useEffect, useState } from "react";
+
+
+// const useServices = () => {
+//     const [services, setServices] = useState([]);
+
+//     useEffect(() => {
+//         fetch('https://car-doctor-server-topaz-one.vercel.app/services')
+//             .then(res => res.json())
+//             .then(data => setServices(data))
+//     }, [])
+
+//     return services;
+// };
+
+// export default useServices;
--- src/components/layout/useAuth.jsx
+++ src/components/layout/useAuth.jsx
@@ -10,3 +10,16 @@ const useAuth = () => {
 };
 
 export default useAuth;
+
+
+
+// import { useContext } from "react";
+// import { AuthContext } from "../providers/AuthProvider";
+
+
+// const useAuth = () => {
+//     const auth = useContext(AuthContext);
+//     return auth;
+// };
+
+// export default useAuth;
